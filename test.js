39d297a5af980fbadeb59a6d5eded04d31074e40
sergey <serge.dmitriev@gmail.com>
2014-08-10 00:53:15 +0800

    add mp3 file

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var YandexASR = require('./index');
 var ya = new YandexASR({
     developer_key: '22d458e6-c881-4b92-b59c-b78e6f88c3e9',
     uuid: '12345678123456781234567812345678',
-    file: 'data/2.mp3',
+    file: 'data/1.mp3',
     filetype: 'audio/x-mpeg-3',
 });
 

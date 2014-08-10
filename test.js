c81adf2578f87ce4105c62d7a0304d1806f84292
sergey <serge.dmitriev@gmail.com>
2014-08-10 13:13:43 +0800

    some changes

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ var yandex_speech = require('./index');
 
 var ya = yandex_speech.ASR({
     developer_key: '22d458e6-c881-4b92-b59c-b78e6f88c3e9',    
-    file: 'data/1.mp3',   
+    file: 'data/hello.mp3',   
 });
 
 ya.recognize(function(err, httpResponse, body){
-    console.log(err, httpResponse, body);
+    console.log(err, httpResponse.statusCode, body);
 });
 
 

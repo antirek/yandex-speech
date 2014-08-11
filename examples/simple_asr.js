f772f83c89debebf4cf729f8402d1a0551925b6a
Dmitriev Sergey <antirek@users.noreply.github.com>
2014-08-11 15:24:06 +0800

    Update simple_asr.js

--- examples/simple_asr.js
+++ examples/simple_asr.js
@@ -1,9 +1,13 @@
 var yandex_speech = require('../index');
 
 yandex_speech.ASR({
-    developer_key: '22d458e6-c881-4b92-b59c-b78e6f88c3e9',    
+    developer_key: '22d458e6-c881-4b92-b59c-b78e6f88c3e9',   
     file: 'data/1.mp3',
-	}, function(err, httpResponse, body){
-    	console.log(err, httpResponse.statusCode, body);
-	}
+	}, function(err, httpResponse, xml){
+		if(err){
+    		console.log(err);
+    	}else{
+    		console.log(httpResponse.statusCode, xml)
+    	}
+    }
 );

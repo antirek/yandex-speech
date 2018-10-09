49610d832335706d43199404ec45193677dbf05c
Glacial <kli-van@yandex.ru>
2018-10-09 22:05:54 +0300

    добавил в tts недостающие параметры и убрал лишние согласно документации (включая ключ разработчика), а также добавил параметр disableAntimat в asr

--- examples/simple_tts.js
+++ examples/simple_tts.js
@@ -1,9 +1,10 @@
 var yandex_speech = require('../index');
 
 yandex_speech.TTS({
+	developer_key: '3b7b9fba-cbcd-47d1-854a-b359ca0e5da7',
 	text: 'Привет, мир!',
 	file: 'data/hello.mp3'
-	}, function(){
-		console.log('done');
-	}
+}, function () {
+	console.log('done');
+}
 );
